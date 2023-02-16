3be1c5466d11c0f08f8c518803c481356ba83595
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-02-15 19:16:29 -0800

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="mx-auto min-h-screen items-center justify-center ">
+      <div className="p-56">
+        <h1 className="text-center text-3xl font-bold">Hello World!</h1>
+      </div>
+    </main>
+  )
+}
